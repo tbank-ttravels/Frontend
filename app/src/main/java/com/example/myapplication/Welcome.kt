9ae6076d6dd98c8d4f7d019241dd5d78aee17b0b
pavlova1221 <holmberggg12@gmail.com>
2025-12-12 03:02:08 +0400

    соединение viewmodel

--- app/src/main/java/com/example/myapplication/Welcome.kt
+++ app/src/main/java/com/example/myapplication/Welcome.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
 
+
 @Composable
 fun Welcome(navController: NavController) {
     LaunchedEffect(Unit) {
